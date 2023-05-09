b45193349b4d8a76660d579e0d9c57fb568fc044
mondlivundla <mondlivundla7@gmail.com>
2023-05-10 00:49:30 +0200

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" recursive function that queries the Reddit API"""
+"""
+recursive function that queries the Reddit API
+"""
 import requests
 import sys
 after = None
@@ -7,12 +9,16 @@ count_dic = []
 
 
 def count_words(subreddit, word_list):
-    """parses the title of all hot articles, and prints a sorted count of given
-    keywords (case-insensitive, delimited by spaces) """
+    """
+    parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces)
+    """
     global after
     global count_dic
     headers = {'User-Agent': 'xica369'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'after': after}
     response = requests.get(url, headers=headers, allow_redirects=False,
-                            params=parameters)
+            params=parameters)
+
+    return response
